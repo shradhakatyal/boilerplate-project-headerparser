5e5afaf6d6583311a8216bda94cb76d8260b0dd1
shradhakatyal <skatyal2204@gmail.com>
2018-11-10 22:43:36 +0530

    new route added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var port = 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -24,9 +25,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  res.json({ip: req.connection.remoteAddress, language: req.headers["accept-language"], system: req.headers["user-agent"]});
+});
+
+app.get("*", function(req, res) {
+  res.json({error: 'Page not found'});
+});
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port , function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
